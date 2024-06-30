8db75f4f841b3c7631b348c63ade5bf16668626f
Rakesh Kumar <119156077+rakeshkumar04@users.noreply.github.com>
2024-06-30 18:59:43 +0530

    Create Missing-And-Repeating.java

---
+++ Array/Hard/Missing-And-Repeating.java
@@ -0,0 +1,30 @@
+class Solve {
+    int[] findTwoElement(int arr[], int ni) {
+        // code here
+        
+        long n = ni;
+        long sn = (n*(n+1))/2;
+        long s2n = (n*(n+1)*(2*n+1))/6;
+        
+        long s = 0;
+        long s2 = 0;
+        
+        for(int i=0; i<n; i++){
+            s+=arr[i];
+            s2+=(long)arr[i]*(long)arr[i];
+        }
+        
+        long val1 = s-sn;
+        long val2 = s2-s2n;
+        val2 = val2 / val1;
+        
+        long x = (val1 + val2)/2;
+        
+        long y = x - val1;
+        
+        int [] ans = {(int)x,(int)y};
+        
+        return ans;
+        
+    }
+}
